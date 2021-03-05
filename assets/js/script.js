51cc6c7dc0d18c6e3d63f1d73ea75599079e9c4c
Emily Pozzi <epozzi5@yahoo.com>
2021-03-05 13:06:26 -0600

    form submit with no information

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form")
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
+var createTaskHandler = function(event) {
+
+  event.preventDefault();
+
+  var listItemEl = document.createElement("li"); //creating a list item. 
+  listItemEl.className = "task-item"; //creating a class for the list item
+  listItemEl.textContent = "This is a new task."; // in that list items class you are placing text content that says, "This is a  new task"
+  tasksToDoEl.appendChild(listItemEl); // taskToDoEl is using the unordered list in HTML. Assigning the appendChild. Adding the variable to be used in the function  
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
